3bf4a32ea021a7a73e503101cc029033557559f0
Takashi Imagire <imagire@gmail.com>
2018-08-03 22:18:13 +0900

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,17 @@ int main(int argc, char *argv[])
     list *header =  NULL;
     
     while(fgets(str, sizeof(str), stdin)){
-        if(str[0]=='-' && strlen(str)==2){
+        int n = atoi(str);
+        if(n == 0){
+            // リストを表示
+            list *p = header;
+            while(p){
+                printf("%d", p->value);
+                p = p->next;
+                if(p){printf(",");}
+            }
+            printf("\n");
+        }else if(n ==-1){
             // 先頭を削除
             list *p = header;
             if(p){
@@ -22,24 +32,12 @@ int main(int argc, char *argv[])
                 free(p);
             }
         }else{
-            int n = atoi(str);
-            if(n==0){
-                // リストを表示
-                list *p = header;
-                while(p){
-                    printf("%d", p->value);
-                    p = p->next;
-                    if(p){printf(",");}
-                }
-                printf("\n");
-            }else{
-                // 要素を追加
-                list *p = (list*)malloc(sizeof(list));
-                if(p){
-                    p->next = header;
-                    p->value = n;
-                    header = p;
-                }
+            // 要素を追加
+            list *p = (list*)malloc(sizeof(list));
+            if(p){
+                p->next = header;
+                p->value = n;
+                header = p;
             }
         }
     }
