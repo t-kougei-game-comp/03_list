79d195c31613dc3595f71e5ba546af7f5a99490a
Takashi Imagire <imagire@gmail.com>
2018-08-03 21:46:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct list{
+    list *next;
+    int value;
+};
 
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    list *header = nullptr;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        if(strcmp(str,"-")==0){// 先頭を削除
+            list *p = header;
+            if(p){
+                printf("%d\n", p->value);
+                header = p->next;
+                free(p);
+            }
+        }else{
+            int n = atoi(str);
+            if(n==0){// リストを表示
+                list *p = header;
+                while(p){
+                    printf("%d", p->value);
+                    p = p->next;
+                    if(p){printf(",");}
+                }
+                printf("\n");
+            }else{
+                // 要素を追加
+                list *p = (list*)malloc(sizeof(list));
+                if(p){
+                    p->next = header;
+                    p->value = n;
+                    header = p;
+                }
+            }
+        }
     }
 
     return 0;
